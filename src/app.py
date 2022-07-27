1fc51c68a79cd63efe150d77f99d2b2147b8371e
akhilATC <akhiltcheriyan002@gamil.com>
2022-07-28 00:52:59 +0530

    changes

--- src/app.py
+++ src/app.py
@@ -2,20 +2,26 @@ import streamlit as st
 from bid_parser import parse
 
 
-def submit_data(proto,str_):
-    print(f"protocol is {proto}")
-    print(str_)
-    resp = parse(proto,str_)
-    if resp:
-        st.success(resp)
-    else:
-        st.error(f"Failed to fetch...:) error = {resp[1]}")
-
+def submit_data():
+    print("--submit--")
+    vals = list(filter(None, [x for x in st.session_state.values()]))
+    print(len(vals))
+    if len(vals) == 3:
+        proto = st.session_state['protocol']
+        exchange = st.session_state['exchange']
+        stream = st.session_state['code_string']
+        resp = parse(proto, stream)
+        st.info(resp)
+        st.session_state.clear()
 
 st.title("bidify 🛩️")
 st.subheader("Bid simulator")
 st.write("Generate bid request in google/openrtb protocol :)")
+protocol = st.radio("Protocol:", ['Google', 'Openrtb'], key="protocol")
+exchange = st.selectbox('choose a exchange', ['GOOGLE', 'OPENX'], key="exchange")
+code_string = st.text_area(label="Request string", key="code_string") or None
+st.button(label="Generate", on_click=submit_data)
 
-protocol = st.radio("Protocol:", ['Google', 'Openrtb'])
-code_string = st.text_area(label="Request string") or None
-st.button(label="Generate", on_click=submit_data(protocol, code_string))
+
+
+
--- src/bid_parser.py
+++ src/bid_parser.py
@@ -1,16 +1,20 @@
 import openrtb_pb2 as openrtbBid
 import realtime_bidding_proto_pb2 as googleBid
-from google.protobuf import text_format as tf
+# from google.protobuf import text_format as tf
 from google.protobuf import json_format as jf
 
 
 def parse(protocol, str_):
+    print(type(str_))
     try:
         if protocol == 'Openrtb':
-            b = jf.Parse(jf.MessageToJson(str_), openrtbBid.BidRequest())
+            import json
+            c = json.loads(str_)
+            print(type(c))
+            b = jf.ParseDict(c, openrtbBid.BidRequest())
             return b.SerializeToString()
-        b = tf.Parse(str_, googleBid.BidRequest)
-        return b.SerializeToString()
+        else:
+            b = jf.ParseDict(str_, googleBid.BidRequest)
+            return b.SerializeToString()
     except Exception as e:
-        print(e)
-        return None,e
+        return None, str(e)
